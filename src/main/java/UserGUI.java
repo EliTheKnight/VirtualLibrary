dc69249f006094728e959f792f929544e2f109ee
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2022-12-27 17:35:53 -0500

    here you go Eli

--- src/main/java/UserGUI.java
+++ src/main/java/UserGUI.java
@@ -21,16 +21,34 @@ public class UserGUI {
         JLabel label = new JLabel("Hello World!");
 
         JMenuBar mb = new JMenuBar();
-        constraints.gridy = 20;
         constraints.anchor = GridBagConstraints.NORTH;
-        JMenu fileMenu = new JMenu("File");
-        JMenu viewMenu = new JMenu("View");
+        constraints.gridwidth = GridBagConstraints.REMAINDER;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weighty = 0.5;
+        constraints.weightx = 1.0;
 
-        JMenuItem openFileMenuItem = new JMenuItem("Open...");
-        openFileMenuItem.addActionListener(e -> System.out.println("Hello World!"));
-        fileMenu.add(openFileMenuItem);
+        JMenu fileMenu = new JMenu("File");
+        JMenu libraryMenu = new JMenu("Library");
+
+        JMenuItem preferencesFileMenuItem = new JMenuItem("Preferences...");
+        preferencesFileMenuItem.addActionListener(e -> System.out.println("Displaying preferences."));
+        fileMenu.add(preferencesFileMenuItem);
+
+        JMenuItem logoutFileMenuItem = new JMenuItem("Logout");
+        logoutFileMenuItem.addActionListener(e -> System.out.println("Logging out."));
+        fileMenu.add(logoutFileMenuItem);
 
         mb.add(fileMenu);
+
+        JMenuItem loadLibraryLibraryMenuItem = new JMenuItem("Load library...");
+        loadLibraryLibraryMenuItem.addActionListener(e -> System.out.println("Loading library."));
+        libraryMenu.add(loadLibraryLibraryMenuItem);
+
+        JMenuItem openCheckedOutMenuItem = new JMenuItem("Open Checked Out Books");
+        openCheckedOutMenuItem.addActionListener(e -> System.out.println("Opening checked out books."));
+        libraryMenu.add(openCheckedOutMenuItem);
+
+        mb.add(libraryMenu);
 
         this.frame.add(mb, constraints);
         this.frame.setVisible(true);
